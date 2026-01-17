e68501cb04aadc1007ec9fa903cc786453fe0169
Ishan Rawat <rawatishan510@gmail.com>
2026-01-17 22:45:08 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+def calculator():
+    x = int(input("enter any number"))
+    y = int(input("enter any number"))
+    z = input("choose A, B, C, D for addition, subtraction, multiplication and division")
+    
+    if (z=="A"):
+        print(x+y)
+    elif(z=="B"):
+        print(x-y)
+    elif(z=="C"):
+        print(x*y)
+    elif(z=="D"):
+        print(x/y)
+    else:
+        pass
+
+calculator()
